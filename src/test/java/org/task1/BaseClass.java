7222c0ffce1836a0c64b8f139878cc9062118dad
sprisov <sprinsov@gmail.com>
2022-09-19 23:20:13 +0530

    new code

--- src/test/java/org/task1/BaseClass.java
+++ src/test/java/org/task1/BaseClass.java
@@ -1,5 +1,23 @@
 package org.task1;
 
 public class BaseClass {
+	
+	public void method4() {
+		System.out.println("1");
+
+	}
+	
+	
+	public void method5()
+	
+	{
+		System.out.println("2");
+		
+	}
+	
+	public void method6() {
+		System.out.println("3");
+
+	}
 
 }
